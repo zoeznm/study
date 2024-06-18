27ea0286e8f25b050329b1d765138b9496feea39
zoeznm <bomikim046@gmail.com>
2024-06-18 23:04:39 +0900

    database

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
+const querystring = require('querystring');
 
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)");
+  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
 });
 
 const server = http.createServer((req, res) => {
@@ -29,60 +30,22 @@ const server = http.createServer((req, res) => {
       });
     }
   } else if (req.method === 'POST') {
-    if (req.url === '/users') {
+    if (req.url === '/login') {
       let body = '';
       req.on('data', chunk => {
         body += chunk.toString();
       });
       req.on('end', () => {
-        const user = JSON.parse(body);
-        const stmt = db.prepare("INSERT INTO users (name, email) VALUES (?, ?)");
-        stmt.run(user.name, user.email, function(err) {
+        const { username, password } = querystring.parse(body);
+        const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        stmt.run(username, password, function(err) {
           if (err) {
             res.writeHead(500, {'Content-Type': 'text/plain'});
             res.end('Internal Server Error');
             return;
           }
           res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-          res.end(JSON.stringify({ id: this.lastID }));
-        });
-        stmt.finalize();
-      });
-    } else if (req.url === '/updateUser') {
-      let body = '';
-      req.on('data', chunk => {
-        body += chunk.toString();
-      });
-      req.on('end', () => {
-        const user = JSON.parse(body);
-        const stmt = db.prepare("UPDATE users SET email = ? WHERE name = ?");
-        stmt.run(user.email, user.name, function(err) {
-          if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
-            return;
-          }
-          res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-          res.end(JSON.stringify({ changes: this.changes }));
-        });
-        stmt.finalize();
-      });
-    } else if (req.url === '/deleteUser') {
-      let body = '';
-      req.on('data', chunk => {
-        body += chunk.toString();
-      });
-      req.on('end', () => {
-        const user = JSON.parse(body);
-        const stmt = db.prepare("DELETE FROM users WHERE name = ?");
-        stmt.run(user.name, function(err) {
-          if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
-            return;
-          }
-          res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-          res.end(JSON.stringify({ changes: this.changes }));
+          res.end(JSON.stringify({ id: this.lastID, username, message: 'User registered successfully!' }));
         });
         stmt.finalize();
       });
@@ -93,4 +56,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
-
